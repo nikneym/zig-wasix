25a1eb3523a88b3bca4f702a3091eace59b992f9
nikneym <halildrk@gmail.com>
2025-01-23 04:09:55 +0300

    chore: more zig package manager changes

--- build.zig
+++ build.zig
@@ -15,37 +15,11 @@ pub fn build(b: *std.Build) !void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    // Expose zig-wasix as a module.
+    _ = b.addModule("wasix", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .imports = &.{},
     });
-
-    // Modules can depend on one another using the `std.Build.Module.addImport` function.
-    // This is what allows Zig source code to use `@import("foo")` where 'foo' is not a
-    // file path. In this case, we set up `exe_mod` to import `lib_mod`.
-    lib_mod.addImport("wasix", lib_mod);
-
-    //const lib = b.addStaticLibrary(.{
-    //    .name = "wasix",
-    //    // In this case the main source file is merely a path, however, in more
-    //    // complicated build scripts, this could be a generated file.
-    //    .root_source_file = b.path("src/main.zig"),
-    //    .target = target,
-    //    .optimize = optimize,
-    //});
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    //b.installArtifact(lib);
 
     // In order to test, we output a .wasm file that can be run with wasmer.
     // zig build -Dtarget=wasm32-wasi
